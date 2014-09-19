4f0757cb1b509093981501c25166b889d57ff836
andy fang <andy@mingtak.com.tw>
2014-09-19 09:54:34 +0100

    initial version

---
+++ mingtak/securitymethod/tests.py
@@ -0,0 +1,58 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import mingtak.securitymethod
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['mingtak.securitymethod'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              mingtak.securitymethod)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='mingtak.securitymethod',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='mingtak.securitymethod.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt',
+            package='mingtak.securitymethod',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        # -*- extra stuff goes here -*-
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
